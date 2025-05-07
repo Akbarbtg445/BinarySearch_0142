5393e82180f1aa7541f20ee7f39c518463ce3b63
M. Ridho Akbar <129049803+Akbarbtg445@users.noreply.github.com>
2025-05-07 13:25:49 +0700

    Membuat Input

---
+++ Bs.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int element[10];
+int nPanjang;
+int x;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukan banyaknya elemen pada array (maksimal 10):";
+        cin >> nPanjang;
+        if (nPanjang <=10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+        }
+        {
+            cout << "\n==============================";
+            cout << "      Masukan Elemen Array      ";
+            cout << "================================";
+            for (int i=0; i <nPanjang;i++)
+            {
+                cout << "Data ke-"<< (i +1) << "=";
+                cin >> element[i];
+            }
+        }
+    }
+}
